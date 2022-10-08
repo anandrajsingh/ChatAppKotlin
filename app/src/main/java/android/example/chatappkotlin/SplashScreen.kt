a93dc0363ac70894ef9b6dc9ab372cedd784d5d2
Anand Raj Singh <singhpariharanandraj@gmail.com>
2022-10-08 21:16:41 +0530

    status bar and action bar changed

--- app/src/main/java/android/example/chatappkotlin/SplashScreen.kt
+++ app/src/main/java/android/example/chatappkotlin/SplashScreen.kt
@@ -9,5 +9,7 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
     }
 }
