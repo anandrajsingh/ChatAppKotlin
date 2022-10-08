39b7b7adf36963b41215ebd4a758cc57e43216a9
Anand Raj Singh <singhpariharanandraj@gmail.com>
2022-10-08 18:35:52 +0530

    Splash Screen

--- app/src/main/java/android/example/chatappkotlin/LoginActivity.kt
+++ app/src/main/java/android/example/chatappkotlin/LoginActivity.kt
@@ -1,18 +1,31 @@
 package android.example.chatappkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var edtEmail :EditText
     private lateinit var edtPassword :EditText
-    private lateinit var btnLogin :EditText
-    private lateinit var btnSignUp :EditText
+    private lateinit var btnLogin : CardView
+    private lateinit var btnSignUp : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        edtEmail = findViewById(R.id.email)
+        edtPassword = findViewById(R.id.password)
+        btnLogin = findViewById(R.id.login_button)
+        btnSignUp = findViewById(R.id.sign_up)
+
+        btnSignUp.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
