72c7adf369c00d7e54d7a0635b5284a396185ec5
Anand Raj Singh <singhpariharanandraj@gmail.com>
2022-10-09 20:44:20 +0530

    Connected to Firebase

--- app/src/main/java/android/example/chatappkotlin/SplashScreen.kt
+++ app/src/main/java/android/example/chatappkotlin/SplashScreen.kt
@@ -1,8 +1,10 @@
 package android.example.chatappkotlin
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
@@ -10,6 +12,11 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
 
     }
 }
